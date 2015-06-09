4f57759c0caff605767530873518d1860e746547
oktobercomstock <oktober.comstock@gmail.com>
2015-06-09 15:47:12 +0000

    Posts & Questions Tabs

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :advertisements
   resources :posts
   get 'about' => 'welcome#about'
+  get 'posts' => 'welcome#posts'
+  get 'questions' => 'welcome#questions'
   
   root to: 'welcome#index'
 end
