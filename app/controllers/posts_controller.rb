3d157309152f31724f8472772c08187cde9766fb
oktobercomstock <oktober.comstock@gmail.com>
2015-06-12 05:20:16 +0000

    Authorization Lesson

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,30 @@
 class PostsController < ApplicationController
   def index
    @posts = Post.all
+   authorize @posts
   end
 
   def show
    @post = Post.find(params[:id])
+   authorize @post
   end
 
   def new
    @post = Post.new
+   authorize @post
   end
   
   def create
-  @post = current_user.posts.build(params.require(:post).permit(:title, :body))
-   if @post.save
+    @post = current_user.posts.build(params.require(:post).permit(:title, :body))
+    authorize @post
+    if @post.save
     flash[:notice] = "Post was saved."
-    redirect_to @post
+   redirect_to @post
     
    else
     flash[:error] = "There was an error saving the post. Please try again."
     render :new
-   end
+    end
   end
 
   def edit
@@ -28,11 +32,12 @@ class PostsController < ApplicationController
   end
   
   def update
-     @post = Post.find(params[:id])
-     if @post.update_attributes(params.require(:post).permit(:title, :body))
-       flash[:notice] = "Post was updated."
+    @post = Post.find(params[:id])
+    authorize @post
+   if @post.update_attributes(params.require(:post).permit(:title, :body))
+      flash[:notice] = "Post was updated."
        redirect_to @post
-     else
+   else
        flash[:error] = "There was an error saving the post. Please try again."
        render :edit
      end
