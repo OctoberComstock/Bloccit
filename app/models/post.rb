f4374e632472def05438a9e04e7a948c70f47b02
oktobercomstock <oktober.comstock@gmail.com>
2015-06-25 01:39:31 +0000

    Validation Lesson

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,9 +5,11 @@ class Post < ActiveRecord::Base
     has_one :summary
     
     default_scope { order('created_at DESC') }
-    scope :ordered_by_title, -> { reorder(title: :asc) }
-    scope :ordered_by_reverse_created_at, -> { reorder(created_at: :asc) }
     
+   validates :title, length: { minimum: 5 }, presence: true
+   validates :body, length: { minimum: 20 }, presence: true
+   validates :topic, presence: true
+   validates :user, presence: true
 end
 
 
