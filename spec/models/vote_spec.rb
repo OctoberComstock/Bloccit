9864adcbd8b3900d8d201bf0260f1af3b50b1039
oktobercomstock <oktober.comstock@gmail.com>
2015-07-13 00:36:49 +0000

    First passing tests!

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
-
-RSpec.describe Vote, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+ 
+describe Vote do
+  
+  before do
+    @upvote = Vote.new(value: 1)
+    @downvote = Vote.new(value: -1)
+  end
+  
+  describe "validations" do
+    describe "value validation" do
+      it "allows 1 as value" do
+        expect( @upvote.value ).to eq(1)
+      end
+       it "allows -1 value" do
+        expect( @downvote.value ).to eq(-1)
+      end
+    end
+  end
 end
